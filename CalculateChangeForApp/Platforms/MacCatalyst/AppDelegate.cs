921062dee50995c6ea3a205dd4bdb3c2c7307233
Bikash Chhantyal <bikashchhantyal2@gmail.com>
2023-05-29 17:02:19 -0400

    Initialized project for the app.

---
+++ CalculateChangeForApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CalculateChangeForApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
